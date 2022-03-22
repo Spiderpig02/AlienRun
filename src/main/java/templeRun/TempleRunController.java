650901e0657cc3db6b397eae06c94befb4f03311
Daniel N Hansen <danielhansen1000@hotmail.com>
2022-03-22 15:57:04 +0100

    fixed the chrash bug?

--- src/main/java/templeRun/TempleRunController.java
+++ src/main/java/templeRun/TempleRunController.java
@@ -5,8 +5,9 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
-public class TempleRunController {
+public class TempleRunController extends AnchorPane{
 
     @FXML
     private Canvas gameCanvas;
