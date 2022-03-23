bd8200ae603a5d6c3088fe77b4be679427d8a5a5
Daniel N Hansen <danielhansen1000@hotmail.com>
2022-03-23 14:20:11 +0100

    ting

--- src/main/java/templeRun/TempleRun.java
+++ src/main/java/templeRun/TempleRun.java
@@ -1,8 +1,15 @@
 package templeRun;
 
-public class TempleRun {
+public class TempleRun implements Runnable {
     
 
+    private Thread thread = new Thread(this);
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
     
 
 
--- src/main/java/templeRun/TempleRunController.java
+++ src/main/java/templeRun/TempleRunController.java
@@ -2,17 +2,25 @@ package templeRun;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
-public class TempleRunController extends AnchorPane{
+public class TempleRunController extends AnchorPane {
 
     @FXML
     private Canvas gameCanvas;
     private Button startButton;
     private TextField username;
     private ListView<Score> scoreboard;
+
+    public void startGame() {
+        GraphicsContext gc = gameCanvas.getGraphicsContext2D();
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, 250, 400);
+    }
 
 }
